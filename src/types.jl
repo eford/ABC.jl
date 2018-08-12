68a1ed5cbd894c244ba6a91e791eacfc83fa25ff
Eric Ford <eford@psu.edu>
2018-08-12 04:08:14 -0400

    adding ability to compute distance multiple times to improve accurancy of acceptance probability

--- src/types.jl
+++ src/types.jl
@@ -12,6 +12,7 @@ type abc_pmc_plan_saveable_type <: abc_plan_type
    num_max_attempt::Int64
    num_max_attempt_valid::Int64
    num_max_times::Int64
+   num_dist_per_obs::Int64
    epsilon_init::Float64
    init_epsilon_quantile::Float64
    epsilon_reduction_factor::Float64
@@ -39,6 +40,7 @@ type abc_pmc_plan_type <: abc_plan_type
    num_max_attempt::Int64
    num_max_attempt_valid::Int64
    num_max_times::Int64
+   num_dist_per_obs::Int64
    epsilon_init::Float64
    init_epsilon_quantile::Float64
    epsilon_reduction_factor::Float64
@@ -56,7 +58,7 @@ type abc_pmc_plan_type <: abc_plan_type
    function abc_pmc_plan_type(gd::Function,css::Function,cd::Function,p::Distribution;
      make_proposal_dist::Function = make_proposal_dist_gaussian_full_covar, param_active::Vector{Int64} = collect(1:length(Distributions.rand(p))),
      normalize::Function = noop, is_valid::Function = noop,
-     num_part::Integer = 10*length(Distributions.rand(p))^2, num_max_attempt::Integer = 1000, num_max_attempt_valid::Integer = 100, num_max_times::Integer = 100,
+     num_part::Integer = 10*length(Distributions.rand(p))^2, num_max_attempt::Integer = 1000, num_max_attempt_valid::Integer = 100, num_max_times::Integer = 100, num_dist_per_obs::Int64 = 1, 
      epsilon_init::Float64 = 1.0, init_epsilon_quantile::Float64 = 0.75, epsilon_reduction_factor::Float64 = 0.9,
      target_epsilon::Float64 = 0.01, tau_factor::Float64 = 2.0,
      adaptive_quantiles::Bool = false, stop_on_decreasing_efficiency::Bool = false,
@@ -65,23 +67,24 @@ type abc_pmc_plan_type <: abc_plan_type
      @assert(num_max_attempt>=1)
      @assert(num_max_attempt_valid>=1)
      @assert(num_max_times>0)
+     @assert(1<=num_dist_per_obs<=10000)
      @assert(epsilon_init>0.01)
      @assert(0<init_epsilon_quantile<=1.0)
      @assert(0.5<epsilon_reduction_factor<1.0)
      @assert(target_epsilon>0.0)
      @assert(1.0<=tau_factor<=4.0)
      @assert(0.0<=frac_rejects_save<=1.0)
-     new(gd,css,cd,p,make_proposal_dist,normalize,is_valid, num_part,num_max_attempt,num_max_attempt_valid,num_max_times,epsilon_init, init_epsilon_quantile,epsilon_reduction_factor,target_epsilon,tau_factor,frac_rejects_save,param_active,adaptive_quantiles,stop_on_decreasing_efficiency,save_params,save_summary_stats,save_distances,in_parallel)
+     new(gd,css,cd,p,make_proposal_dist,normalize,is_valid, num_part,num_max_attempt,num_max_attempt_valid,num_max_times,num_dist_per_obs,epsilon_init, init_epsilon_quantile,epsilon_reduction_factor,target_epsilon,tau_factor,frac_rejects_save,param_active,adaptive_quantiles,stop_on_decreasing_efficiency,save_params,save_summary_stats,save_distances,in_parallel)
    end
 end
 
 function abc_pmc_plan_saveable_type(plan::abc_pmc_plan_type)
-  abc_pmc_plan_saveable_type(string(plan.gen_data),string(plan.calc_summary_stats),string(plan.calc_dist),plan.prior,string(plan.make_proposal_dist),string(plan.normalize),string(plan.is_valid),plan.num_part,plan.num_max_attempt,plan.num_max_attempt_valid,plan.num_max_times,plan.epsilon_init,plan.init_epsilon_quantile,plan.epsilon_reduction_factor,plan.target_epsilon,plan.tau_factor,plan.frac_rejects_save,plan.param_active,plan.adaptive_quantiles,plan.stop_on_decreasing_efficiency,plan.save_params,plan.save_summary_stats,plan.save_distances,plan.in_parallel)
+  abc_pmc_plan_saveable_type(string(plan.gen_data),string(plan.calc_summary_stats),string(plan.calc_dist),plan.prior,string(plan.make_proposal_dist),string(plan.normalize),string(plan.is_valid),plan.num_part,plan.num_max_attempt,plan.num_max_attempt_valid,plan.num_max_times,plan.num_dist_per_obs,plan.epsilon_init,plan.init_epsilon_quantile,plan.epsilon_reduction_factor,plan.target_epsilon,plan.tau_factor,plan.frac_rejects_save,plan.param_active,plan.adaptive_quantiles,plan.stop_on_decreasing_efficiency,plan.save_params,plan.save_summary_stats,plan.save_distances,plan.in_parallel)
 end
 
 # WARNING: This doesn't really work yet.  There seems to be some issue with the scope of the saved function and what's currently imported
 function abc_pmc_plan_type(plan::abc_pmc_plan_saveable_type)
-  abc_pmc_plan_saveable_type(eval(Symbol(plan.gen_data)),eval(Symbol(plan.calc_summary_stats)),eval(Symbol(plan.calc_dist)),plan.prior,eval(Symbol(plan.make_proposal_dist)),eval(Symbol(plan.normalize)),eval(Symbol(plan.is_valid)),plan.num_part,plan.num_max_attempt,plan.num_max_attempt_valid,plan.num_max_times,plan.epsilon_init,plan.init_epsilon_quantile,plan.epsilon_reduction_factor,plan.target_epsilon,plan.tau_factor,plan.frac_rejects_save,plan.param_active,plan.adaptive_quantiles,plan.stop_on_decreasing_efficiency,plan.save_params,plan.save_summary_stats,plan.save_distances,plan.in_parallel)
+  abc_pmc_plan_saveable_type(eval(Symbol(plan.gen_data)),eval(Symbol(plan.calc_summary_stats)),eval(Symbol(plan.calc_dist)),plan.prior,eval(Symbol(plan.make_proposal_dist)),eval(Symbol(plan.normalize)),eval(Symbol(plan.is_valid)),plan.num_part,plan.num_max_attempt,plan.num_max_attempt_valid,plan.num_max_times,plan.num_dist_per_obs,plan.epsilon_init,plan.init_epsilon_quantile,plan.epsilon_reduction_factor,plan.target_epsilon,plan.tau_factor,plan.frac_rejects_save,plan.param_active,plan.adaptive_quantiles,plan.stop_on_decreasing_efficiency,plan.save_params,plan.save_summary_stats,plan.save_distances,plan.in_parallel)
 end
 
 
