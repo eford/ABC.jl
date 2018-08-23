b012bf9e74047b36fb247d56a9fbe349fc629839
autodocs <autodocs>
2018-08-23 05:03:17 +0000

    build based on 85b874a

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "title": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#ABC.jl:-Approximate-Bayesian-Computing-for-Julia-1",
+    "page": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "title": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "category": "section",
+    "text": "This package provides for Approximate Bayesian Computing (ABC) via sequential importance sampling in Julia.   Currently, it implements a single algorithm, ABC-PMC based on Beaumont et al. 2002 via abc_pmc_plan_type. However, it does include several extra features that will eventually be documented. "
+},
+
+{
+    "location": "#Getting-Started-1",
+    "page": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "title": "Getting Started",
+    "category": "section",
+    "text": "Add the ABC package (update once become registered)\nusing ABC\ninclude(\"tests/runtests.jl\")\nSee test1.jl for a demo of how to use ABC"
+},
+
+{
+    "location": "#API-1",
+    "page": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "title": "API",
+    "category": "section",
+    "text": "ABC\nABC.abc_pmc_plan_type(gd::Function,css::Function,cd::Function,p)\nABC.abc_population_type(num_param::Integer, num_particles::Integer)"
+},
+
+{
+    "location": "#Index-1",
+    "page": "ABC.jl:  Approximate Bayesian Computing for Julia",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
